98cf32702a8179a5325d8e34805e56f14d6b3aef
Karem3110 <karemro60@gmail.com>
2022-10-21 10:45:54 +1100

    second commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+console.log ("hello");
+
+function codingBoss() {
+    //This is an alert for when the codingBoss function is called
+    alert ("update it");
+    document.querySelector('.card').style.backgroundColor ="green";
+    document.querySelector('body').style.backgroundColor ="black";
+    document.querySelector('h1').style.color="white";
+}
+codingBoss ();
